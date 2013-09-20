0acd62543f48ed8726a9b95c2b6e183186169e1e
Richard Lee <dlackty@gmail.com>
2013-09-21 05:09:38 +0800

    Enhance error handling and logging.

--- main.go
+++ main.go
@@ -44,14 +44,20 @@ func (h HipChatSender) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
   var n Notification
   dec := json.NewDecoder(r.Body)
-  dec.Decode(&n)
+  err := dec.Decode(&n)
 
-  fmt.Printf("Received notification room_id:%v notification:%+v", room_id, n)
+  if (err != nil) {
+    http.Error(w, "Invalid JSON.", http.StatusBadRequest)
+    return
+  }
+
+  fmt.Printf("Received notification room_id:%v notification:%+v\n", room_id, n)
 
   if len(n.Message) != 0 && len(n.Subject) != 0 {
     err := h.SendMessage(room_id, fmt.Sprintf("%v: %v", n.Subject, n.Message))
     if err != nil {
       fmt.Printf("HipChat error: %v\n", err)
+      http.Error(w, err.Error(), http.StatusInternalServerError)
     }
   }
 }
